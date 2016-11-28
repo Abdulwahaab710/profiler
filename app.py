d864317387aa19fa2bf5175ad45905d35892220b
Abdulwahaab710 <Abdulwahaab.ahmed710@gmail.com>
2016-11-28 17:13:21 -0500

    handling twitters

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def main():
     # url = raw_input('Enter a URL to Profile')
     url = 'http://abdulwahaab.ca'
     r = requests.get(url)
-    findLinks(r.text)
+    print findLinks(r.text)
 
 
 def getHtml(url):
@@ -31,20 +31,21 @@ def findLinks(html):
     githubs = []
     facebook = []
     for link in soup.findAll('a', href=True):
-        if (link['href'][0] != '#' and link['href'][0] != ''):
-            if('@' in link['href']):
-                print '[*] found an email %s' % link['href']
-                emails.append(link['href'])
-            elif('github' in link['href']):
-                print '[*] found a github %s' % link['href']
-                githubs.append(link['href'])
-            elif('facebook' in link['href']):
-                print '[*] found a facebook %s' % link['href']
-                facebook.append(link['href'])
-            else:
-                links.append(str(link['href']))
-                print '[*] found %s' % links[-1]
-                getHtml(links[-1])
+        if (len(link['href']) > 0):
+            if (link['href'][0] != '#' and link['href'][0] != ''):
+                if('@' in link['href'] and 'twitter' not in link['href']):
+                    print '[*] found an email %s' % link['href']
+                    emails.append(link['href'])
+                elif('github' in link['href']):
+                    print '[*] found a github %s' % link['href']
+                    githubs.append(link['href'])
+                elif('facebook' in link['href']):
+                    print '[*] found a facebook %s' % link['href']
+                    facebook.append(link['href'])
+                else:
+                    links.append(str(link['href']))
+                    print '[*] found %s' % links[-1]
+                    getHtml(links[-1])
     return {
         'links': links,
         'emails': emails,
