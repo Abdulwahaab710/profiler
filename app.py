289fd2943c5a5cec004ee6270138cfff61690bcd
Abdulwahaab710 <Abdulwahaab.ahmed710@gmail.com>
2016-11-28 16:14:34 -0500

    new function getHtml()

--- app.py
+++ app.py
@@ -9,12 +9,18 @@ def main():
     print findLinks(r.text)
 
 
+def getHtml(url):
+    r = requests.get(url)
+    findLinks(r.text)
+
+
 def findLinks(html):
     soup = BeautifulSoup(html)
     links = []
     for link in soup.findAll('a', href=True):
         if (link['href'][0] != '#' and link['href'][0] != ''):
             links.append(str(link['href']))
+            getHtml(links[-1])
     return links
 
 if __name__ == '__main__':
