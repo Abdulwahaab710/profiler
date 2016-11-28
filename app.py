448eea326a99f82ea64b190ba94a3bcf5b772621
Abdulwahaab710 <Abdulwahaab.ahmed710@gmail.com>
2016-11-28 16:38:10 -0500

    print invalid urls

--- app.py
+++ app.py
@@ -6,29 +6,32 @@ def main():
     # url = raw_input('Enter a URL to Profile')
     url = 'http://abdulwahaab.ca'
     r = requests.get(url)
-    print findLinks(r.text)
+    findLinks(r.text)
 
 
 def getHtml(url):
     try:
         r = requests.get(url)
+        findLinks(r.text)
     except requests.exceptions.InvalidSchema:
         r = requests.get(
             str(
                 'http://' + url
             )
         )
-    findLinks(r.text)
+        findLinks(r.text)
+    except requests.exceptions.MissingSchema:
+        print '[*] Invalid url %s' % url
 
 
 def findLinks(html):
     soup = BeautifulSoup(html)
     links = []
     for link in soup.findAll('a', href=True):
-        if (link['href'][0] != '#' and link['href'][0] != '' and link['href'][0] != '/'):
+        if (link['href'][0] != '#' and link['href'][0] != ''):
             links.append(str(link['href']))
             print '[*] found %s' % links[-1]
-            getHtml(links[-1])
+            # getHtml(links[-1])
     return links
 
 if __name__ == '__main__':
